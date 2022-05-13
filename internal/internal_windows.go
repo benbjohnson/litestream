4522c7bce5a7a05505816b444132e4cf3485944d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-05-13 09:51:46 +0900

    implement Fileinfo for Windows and non-Windows

---
+++ internal/internal_windows.go
@@ -0,0 +1,12 @@
+//go:build windows
+
+package internal
+
+import (
+	"os"
+)
+
+// Fileinfo returns syscall fields from a FileInfo object.
+func Fileinfo(fi os.FileInfo) (uid, gid int) {
+	return -1, -1
+}
